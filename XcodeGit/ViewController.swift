369886948c8e5f6161abcdcd06323cbbca888d0e
Ahmed Naser <201700099@student.polytechnic.bh>
2020-11-20 21:30:17 +0300

    Added reverse function.

--- XcodeGit/ViewController.swift
+++ XcodeGit/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         print("viewDidLoad()")
+        print(reverse(text: "viewDidLoad()"))
+    }
+    
+    func reverse(text: String) -> String {
+        return String(text.reversed())
     }
 
 
